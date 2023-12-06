b151892820068692ae965799771f39745dfe366a
Gleb Buyankin <gleb.buyankin@x5.ru>
2023-12-06 23:14:25 +0300

    1

--- custom_components/modbus_server/__init__.py
+++ custom_components/modbus_server/__init__.py
@@ -6,7 +6,7 @@ from pyModbusTCP.server import ModbusServer
 import voluptuous as vol
 
 from homeassistant.const import (
-    CONF_IP_ADDRESS,
+    CONF_HOST,
     CONF_PORT,
     EVENT_HOMEASSISTANT_STOP,
     EVENT_HOMEASSISTANT_START
@@ -21,13 +21,17 @@ _LOGGER: logging.Logger = logging.getLogger(__package__)
 
 
 DOMAIN = "modbus_server"
+CONF_MSG_WAIT = "message_wait_seconds"
+
 
 CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: vol.Schema(
             {
-                vol.Required(CONF_IP_ADDRESS): cv.string,
+                vol.Required(CONF_HOST): cv.string,
                 vol.Required(CONF_PORT): cv.port,
+                vol.Required(CONF_MSG_WAIT): cv.positive_float,
+
             }
         )
     },
@@ -38,14 +42,15 @@ CONFIG_SCHEMA = vol.Schema(
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Activate the Modbus Server integration."""
 
-    conf = config[DOMAIN]
+    conf:dict[str, Any] = config[DOMAIN]
 
-    _LOGGER.error(f"{DOMAIN} async_setup {conf[CONF_IP_ADDRESS]}:{conf[CONF_PORT]}")
+    _LOGGER.error(f"{DOMAIN} async_setup {conf[CONF_HOST]}:{conf[CONF_PORT]}")
 
     modbus_server = hass.data[DOMAIN] = ModbusServerManager(
         hass,
-        conf[CONF_IP_ADDRESS],
+        conf[CONF_HOST],
         conf[CONF_PORT],
+        conf.get(CONF_MSG_WAIT, 0.1)
     )
 
     hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, modbus_server.shutdown)
@@ -59,25 +64,28 @@ class ModbusServerManager:
     def __init__(
         self,
         hass,
-        ip_address,
+        host,
         port,
+        msg_wait
     ):
         """Initialize."""
         self._hass = hass
-        self._modbus_server = ModbusServer(ip_address, port, no_block=True)
+        self._modbus_server = ModbusServer(host, port, no_block=True)
         self.__running = False
+        self.__msg_wait = msg_wait
 
     async def start(self, event=None):
         """Start the Kafka manager."""
         _LOGGER.error(f"{DOMAIN} Start ")
 
-        self.__running = True
         self._modbus_server.start()
+        self.__running = True
+
         self.__polling_task = asyncio.create_task(
-            self._network_loop_retry(
-                interval=0.1,
+            self._handle_updates(
+                interval=self.__msg_wait,
             ),
-            name="Updater:start_polling:polling_task",
+            name="ModebusUpdater:start_polling:polling_task",
         )
 
     async def shutdown(self, event=None):
@@ -88,16 +96,17 @@ class ModbusServerManager:
         self.__polling_task.cancel()
         self._modbus_server.stop()
 
-    async def _network_loop_retry(self,  interval: float) -> None:
-        state = [0]
+    async def _handle_updates(self,  interval: float) -> None:
+        state = [0] * 32
         _LOGGER.error(f"{DOMAIN} start _network_loop_retry {self.__running}")
 
         while self.__running:
-            newState = self._modbus_server.data_bank.get_coils(0)
-            if state != newState:
-                state = newState
-                _LOGGER.error(f"{DOMAIN} fire event {newState[0]}")
-                self._hass.bus.fire("modbus_server_event", {'q1':newState[0]})
+            newState = self._modbus_server.data_bank.get_coils(0, number=32)
+            for index, item in enumerate(newState):
+                if state[index] != item:
+                    _LOGGER.error(f"{DOMAIN} fire event {index}:{item}")
+                    self._hass.bus.fire("modbus_server_event", {('q'+index): item})
+            state = newState
             await asyncio.sleep(interval)
 
 
